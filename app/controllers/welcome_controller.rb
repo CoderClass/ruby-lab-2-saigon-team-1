95d83d0801402e66360b9a8249713bee07f2f87e
vtoanb <vtoanb@gmail.com>
2016-10-19 19:38:47 +0700

    doing milestone 1

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,22 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def create
+  	@user = User.new user_params
+  	if @user.save
+  		flash[:success] = "User created successfully"
+  		session[:user_id] = @user.id
+  		redirect_to @user
+  	else
+  		flash[:error] = @user.errors.full_messages.to_sentence
+  		redirect_to new_user_path
+  	end
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :image_url, :password)
+  end
 end
